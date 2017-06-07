ef2573ac5eca4f6adde446695286be7e91ac2682
Cain <350116542@qq.com>
2017-06-07 11:45:02 +0800

    添加OtherFramework

---
+++ OtherFramework/Products/OtherFramework.framework/Headers/OtherFramework.h
@@ -0,0 +1,46 @@
+#//
+//  OtherFramework.h
+//  OtherFramework
+//
+//  Created by Cain on 2017/6/6.
+//  Copyright © 2017年 Cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OtherFramework.
+FOUNDATION_EXPORT double OtherFrameworkVersionNumber;
+
+//! Project version string for OtherFramework.
+FOUNDATION_EXPORT const unsigned char OtherFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OtherFramework/PublicHeader.h>
+
+// SDCycleScrollView
+#import <OtherFramework/UIView+SDExtension.h>
+#import <OtherFramework/SDCycleScrollView.h>
+#import <OtherFramework/SDCollectionViewCell.h>
+#import <OtherFramework/TAAbstractDotView.h>
+#import <OtherFramework/TAAnimatedDotView.h>
+#import <OtherFramework/TADotView.h>
+#import <OtherFramework/TAPageControl.h>
+
+// SDWebImage
+#import <OtherFramework/NSData+ImageContentType.h>
+#import <OtherFramework/NSImage+WebCache.h>
+#import <OtherFramework/SDImageCache.h>
+#import <OtherFramework/SDImageCacheConfig.h>
+#import <OtherFramework/SDWebImageCompat.h>
+#import <OtherFramework/SDWebImageDecoder.h>
+#import <OtherFramework/SDWebImageDownloader.h>
+#import <OtherFramework/SDWebImageDownloaderOperation.h>
+#import <OtherFramework/SDWebImageManager.h>
+#import <OtherFramework/SDWebImageOperation.h>
+#import <OtherFramework/SDWebImagePrefetcher.h>
+#import <OtherFramework/UIButton+WebCache.h>
+#import <OtherFramework/UIImage+GIF.h>
+#import <OtherFramework/UIImage+MultiFormat.h>
+#import <OtherFramework/UIImageView+HighlightedWebCache.h>
+#import <OtherFramework/UIImageView+WebCache.h>
+#import <OtherFramework/UIView+WebCache.h>
+#import <OtherFramework/UIView+WebCacheOperation.h>
